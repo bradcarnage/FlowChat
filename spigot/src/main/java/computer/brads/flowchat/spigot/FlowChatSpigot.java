865f5806cccec68662b8f53d9129668f8fbc672d
Brad <brad@brads.computer>
2026-05-29 09:26:39 -0700

    feat: propagate server/proxy parity from 1.21.4

--- spigot/src/main/java/computer/brads/flowchat/spigot/FlowChatSpigot.java
+++ spigot/src/main/java/computer/brads/flowchat/spigot/FlowChatSpigot.java
@@ -2,9 +2,12 @@ package computer.brads.flowchat.spigot;
 
 import com.github.retrooper.packetevents.PacketEvents;
 import computer.brads.flowchat.core.FlowChatConfig;
+import computer.brads.flowchat.core.FlowChatTestRunner;
 import computer.brads.flowchat.packet.ChatPacketInterceptor;
 import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public class FlowChatSpigot extends JavaPlugin {
     private FlowChatConfig config;
@@ -18,6 +21,13 @@ public class FlowChatSpigot extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Check enforce-secure-profile
+        if (getServer().getOnlineMode()) {
+            // If online mode, secure profile enforcement may be active
+            getLogger().warning("[FlowChat] Server is in online-mode. Outgoing rule modifications " +
+                    "may fail if enforce-secure-profile=true in server.properties.");
+        }
+
         config = new FlowChatConfig(getDataFolder().toPath());
         config.load();
 
@@ -32,8 +42,10 @@ public class FlowChatSpigot extends JavaPlugin {
             } else if (args.length > 0 && args[0].equalsIgnoreCase("toggle")) {
                 config.setDisabled(!config.isDisabled());
                 sender.sendMessage("\u00a7a[FlowChat] " + (config.isDisabled() ? "Disabled" : "Enabled"));
+            } else if (args.length > 0 && args[0].equalsIgnoreCase("test")) {
+                runSelfTest(sender);
             } else {
-                sender.sendMessage("\u00a7e[FlowChat] Usage: /flowchat <reload|toggle>");
+                sender.sendMessage("\u00a7e[FlowChat] Usage: /flowchat <reload|toggle|test>");
             }
             return true;
         });
@@ -43,4 +55,18 @@ public class FlowChatSpigot extends JavaPlugin {
 
     @Override
     public void onDisable() { PacketEvents.getAPI().terminate(); }
+
+    private void runSelfTest(org.bukkit.command.CommandSender sender) {
+        List<FlowChatTestRunner.TestResult> results = FlowChatTestRunner.runCommonTests();
+        int passed = 0;
+        for (FlowChatTestRunner.TestResult r : results) {
+            if (r.passed) {
+                passed++;
+                sender.sendMessage("\u00a7a  \u2713 " + r.number + ". " + r.name);
+            } else {
+                sender.sendMessage("\u00a7c  \u2717 " + r.number + ". " + r.name + " \u2014 " + r.error);
+            }
+        }
+        sender.sendMessage("\u00a7e[FlowChat Test] " + passed + "/" + results.size() + " passed");
+    }
 }
