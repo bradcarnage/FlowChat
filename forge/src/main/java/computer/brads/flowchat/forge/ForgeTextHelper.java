5ff78322fb7bda5a4af2e9ce7f364183bb93d8f5
Brad <brad@brads.computer>
2026-05-28 23:48:06 -0700

    forge 1.14.4: disable sound (mapping compat)

--- forge/src/main/java/computer/brads/flowchat/forge/ForgeTextHelper.java
+++ forge/src/main/java/computer/brads/flowchat/forge/ForgeTextHelper.java
@@ -1,18 +1,11 @@
 package computer.brads.flowchat.forge;
 
-import net.minecraft.client.Minecraft;
-
 public class ForgeTextHelper {
     public static String formatColors(String text) {
-        return text.replaceAll("&([0-9a-fk-or])", "§$1");
+        return text.replaceAll("&([0-9a-fk-or])", "\u00a7$1");
     }
     public static void playSound(String soundName) {
-        try {
-            Minecraft mc = Minecraft.getInstance();
-            if (mc.player != null) {
-                mc.getSoundHandler().play(net.minecraft.client.audio.SimpleSound.master(
-                    net.minecraft.util.SoundEvents.UI_BUTTON_CLICK, 1.0f));
-            }
-        } catch (Exception ignored) {}
+        // Sound notification not supported on Forge 1.14.4 due to mapping differences
+        // Chat processing still works without sound
     }
 }
