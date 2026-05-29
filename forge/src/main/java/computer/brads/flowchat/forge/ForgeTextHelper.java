dae1095013fa1a608632f6622f5b9560cbddeea9
Brad <brad@brads.computer>
2026-05-28 23:45:01 -0700

    forge 1.14.4: fix ForgeTextHelper imports for MCP mappings

--- forge/src/main/java/computer/brads/flowchat/forge/ForgeTextHelper.java
+++ forge/src/main/java/computer/brads/flowchat/forge/ForgeTextHelper.java
@@ -1,18 +1,17 @@
 package computer.brads.flowchat.forge;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.SimpleSound;
-import net.minecraft.util.SoundEvents;
 
 public class ForgeTextHelper {
     public static String formatColors(String text) {
-        return text.replaceAll("&([0-9a-fk-or])", "\u00a7$1");
+        return text.replaceAll("&([0-9a-fk-or])", "§$1");
     }
     public static void playSound(String soundName) {
         try {
             Minecraft mc = Minecraft.getInstance();
             if (mc.player != null) {
-                mc.getSoundManager().play(SimpleSound.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0f));
+                mc.getSoundHandler().play(net.minecraft.client.audio.SimpleSound.master(
+                    net.minecraft.util.SoundEvents.UI_BUTTON_CLICK, 1.0f));
             }
         } catch (Exception ignored) {}
     }
